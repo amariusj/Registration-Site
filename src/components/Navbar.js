19d02d645ed9b83207ce6bde44513427e9fa5ebe
Amarius Jones <amariusj@gmail.com>
2019-11-18 23:38:33 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
   return(
-    <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-primary">
+    <nav id="navbar" className="navbar navbar-expand-lg navbar-dark fixed-top bg-primary">
       <div className="container d-flex">
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
